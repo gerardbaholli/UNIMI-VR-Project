18becd29d858806dc1502c4e36808a48b03870ee
gerardbaholli <gerardbaholli@live.it>
2022-02-01 17:15:31 +0100

    Enemy 2 movement set only on y axis

--- Assets/Test Arachnophobia/Scripts/SinMovement.cs
+++ Assets/Test Arachnophobia/Scripts/SinMovement.cs
@@ -19,9 +19,8 @@ public class SinMovement : MonoBehaviour
 
         transform.LookAt(target.transform);
         transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
-        transform.position += transform.right * Mathf.Sin(Time.time * 3f) * 0.05f;
         transform.position += transform.up * Mathf.Sin(Time.time * 3f) * 0.01f;
-
+        //transform.position += transform.right * Mathf.Sin(Time.time * 3f) * 0.05f;
     }
 
 }
