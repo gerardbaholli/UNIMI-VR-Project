df9f8e2183c1a964518e40432ed9848acee57617
Gerard Baholli <gerardbaholli@live.it>
2022-02-11 15:58:02 +0100

    Add FSM to enemy

--- Assets/Arachnophobia-Test/Scripts/TestSinMovement.cs
+++ Assets/Arachnophobia-Test/Scripts/TestSinMovement.cs
@@ -7,19 +7,71 @@ public class TestSinMovement : MonoBehaviour
     private TestNexus target;
     private float speed;
 
+    [SerializeField] float distanceToAttack = 2f;
+
+    private FSM fsm;
+    [SerializeField] float fsmUpdateTimer = 0.4f;
+
     private void Start()
     {
         target = FindObjectOfType<TestNexus>();
         speed = GetComponent<TestEnemy>().GetSpeed();
+
+
+        FSMState charge = new FSMState();
+        charge.stayActions.Add(Charge);
+
+        FSMState attack = new FSMState();
+        attack.stayActions.Add(Attack);
+
+        FSMTransition t1 = new FSMTransition(Distance);
+        charge.AddTransition(t1, attack);
+
+        fsm = new FSM(charge);
+
+        StartCoroutine(Patrol());
     }
 
-    private void FixedUpdate()
+    //private void FixedUpdate()
+    //{
+    //    float step = speed * Time.deltaTime;
+
+    //    transform.LookAt(target.transform);
+    //    transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+    //}
+
+    private IEnumerator Patrol()
+    {
+        while (true)
+        {
+            fsm.Update();
+            yield return new WaitForSeconds(fsmUpdateTimer);
+        }
+    }
+
+    /* CONDITIONS */
+    public bool Distance()
+    {
+        Debug.Log((target.transform.position - transform.position).magnitude < distanceToAttack);
+        return (target.transform.position - transform.position).magnitude < distanceToAttack;
+    }
+
+
+    /* ACTIONS */
+    private void Charge()
     {
         float step = speed * Time.deltaTime;
 
         transform.LookAt(target.transform);
         transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
-        transform.position += transform.up * Mathf.Sin(Time.time * 2f) * 0.01f;
+        transform.position += transform.up * Mathf.Sin(Time.time * 3f) * 0.01f;
     }
 
+    private void Attack()
+    {
+        float step = speed * Time.deltaTime;
+
+        transform.LookAt(target.transform);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step * 3);
+    }
 }
